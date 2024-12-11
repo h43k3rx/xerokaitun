fd3943bd03452cbe6065a3f220893209caed3f69
h43k3rx <huysakygaming121@gmail.com>
2024-12-11 12:55:05 +0700

    Update kaitun.lua

--- kaitun.lua
+++ kaitun.lua
@@ -1,13 +1,1 @@
-getgenv().Configs = {
-    ["Team"] = "Pirates", -- or Marines
-    ["FPS Boost"] = true, -- true/false recommend true
-    ["Farm Boss Drops"] = {
-        ["Enable"] = false,
-        ["When x2 Exp Expired"] = false
-    },
-    ["Hop Player Near"] = false,
-    ["Soul Guitar"] = true, -- Auto get soul guitar
-    ["Find Fruit"] = false, -- Will find 1m+ price fruits to open swawn doors and unlock third sea (recommend)
-    ["Cursed Dual Katana"] = true --Auto get CDK
-}
 loadstring(game:HttpGet("https://raw.githubusercontent.com/verudous/Xero-Hub/refs/heads/main/kaitun.lua"))()
